e0a513f35c514ac1783c71a9a87dc9ac212e4de2
Aiko Anatasha Wendiono <145739676+aikoanatashawendiono@users.noreply.github.com>
2025-06-12 22:24:48 +0800

    Delete MODUL 5/MLAdventure directory

--- MODUL 5/MLAdventure/app/src/main/java/com/example/mladventure/Character.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mladventure
-
-data class Character(
-    val name: String,
-    val alias: String,
-    val imageRes: Int,
-    val description: String,
-    val wikiUrl: String
-)
